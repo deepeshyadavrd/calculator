d727d458d533d5347f4d1ea766c2df7741e93519
deepeshyadavrd <raoshsbh6@gmail.com>
2024-07-30 17:00:53 +0530

    script

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const buttons = document.querySelector(".buttons");
 const screen = document.querySelector(".screen");
 
 const audio = document.querySelector("audio");
+
+buttons.addEventListener('click', (e) =>{
+    audio.play();
+})
