34d14be1596be991606e8f293121b211728bedb4
Deepesh Yadav <40716552+deepeshyadavrd@users.noreply.github.com>
2024-10-03 17:44:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const buttons = document.querySelector(".buttons");
-
 const screen = document.querySelector(".screen");
-
 const audio = document.querySelector("audio");
 
 buttons.addEventListener('click', (e) =>{
